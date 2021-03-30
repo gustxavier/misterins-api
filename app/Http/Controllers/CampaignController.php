a1c05e7093bc88a2a603c7f600b991015703cc2c
Gustavo Xavier <gustasv00@gmail.com>
2021-03-30 01:22:06 -0300

    insert campaign groups and add .htaccess

--- app/Http/Controllers/CampaignController.php
+++ app/Http/Controllers/CampaignController.php
@@ -13,17 +13,19 @@ class CampaignController extends Controller
     public function redirect($slug){
 
         $campaign = Campaign::where('slug', $slug)->first();
+
         if(!empty($campaign)){
             $group = CampaignHasWppGroup::where('campaign_id', $campaign->id)->get();
-
+            
             foreach($group as $obj){
-                if($obj->actually_click < $obj->max_click){
+                if($obj->actually_click < $obj->max_click && ($campaign->start <= date('Y-m-d')) && $campaign->end >= date('Y-m-d')){
                     $groupWpp = CampaignHasWppGroup::find($obj->id);
                     $groupWpp->actually_click = $groupWpp->actually_click + 1;
                     $groupWpp->update();
                     return redirect($groupWpp->redirect_link); 
                 }
-            }            
+            } 
+            abort(403);           
         }
         abort(404);
     }
--- database/seeders/CampaignHasWppGroupSeeder.php
+++ database/seeders/CampaignHasWppGroupSeeder.php
@@ -14,7 +14,187 @@ class CampaignHasWppGroupSeeder extends Seeder
      */
     public function run()
     {
-        CampaignHasWppGroup::create(['campaign_id' => 1,'name_group'=>'#1 Zap Ins','redirect_link'=>'https://chat.whatsapp.com/HkfOn9d8x4j0J8SCiKjcmc','order'=>'1', 'max_click'=>'3']);
-        CampaignHasWppGroup::create(['campaign_id' => 1,'name_group'=>'#2 Zap Ins','redirect_link'=>'https://chat.whatsapp.com/GAdFGjy9nF8BwB0SUU9kw9','order'=>'2', 'max_click'=>'3']);
+        
+
+CampaignHasWppGroup::create(['campaign_id' => 1,'name_group'=>'#111 Aniversario MR INS','redirect_link'=>'https://chat.whatsapp.com/JRQv3x7mHQdEVA5neYr3OO','order'=>'1', 'max_click'=>'250']);
+
+CampaignHasWppGroup::create(['campaign_id' => 1,'name_group'=>'#112 Aniversario MR INS','redirect_link'=>'https://chat.whatsapp.com/BX8GdP69Hyz1jjNuKyTtQE','order'=>'2', 'max_click'=>'250']);
+
+CampaignHasWppGroup::create(['campaign_id' => 1,'name_group'=>'#113 Aniversario MR INS','redirect_link'=>'https://chat.whatsapp.com/LIXAk57NSLL7YCuY29QFbP','order'=>'3', 'max_click'=>'250']);
+
+CampaignHasWppGroup::create(['campaign_id' => 1,'name_group'=>'#114 Aniversario MR INS','redirect_link'=>'https://chat.whatsapp.com/DgJxTzNSSlMAXWm3QpCNIz','order'=>'4', 'max_click'=>'250']);
+
+CampaignHasWppGroup::create(['campaign_id' => 1,'name_group'=>'#115 Aniversario MR INS','redirect_link'=>'https://chat.whatsapp.com/FyLifQoLn78DU5U8zCR5Vz','order'=>'5', 'max_click'=>'250']);
+
+CampaignHasWppGroup::create(['campaign_id' => 1,'name_group'=>'#116 Aniversario MR INS','redirect_link'=>'https://chat.whatsapp.com/LJGe196fHKsBde1DTEUoBm','order'=>'6', 'max_click'=>'250']);
+
+CampaignHasWppGroup::create(['campaign_id' => 1,'name_group'=>'#117 Aniversario MR INS','redirect_link'=>'https://chat.whatsapp.com/KpTkUNBIjDS32TajFOOvcM','order'=>'7', 'max_click'=>'250']);
+
+CampaignHasWppGroup::create(['campaign_id' => 1,'name_group'=>'#118 Aniversario MR INS','redirect_link'=>'https://chat.whatsapp.com/DpM1956LBUM8aN1WwLrLBF','order'=>'8', 'max_click'=>'250']);
+
+CampaignHasWppGroup::create(['campaign_id' => 1,'name_group'=>'#119 Aniversario MR INS','redirect_link'=>'https://chat.whatsapp.com/LMgasuvUn35I48bf30sTja','order'=>'9', 'max_click'=>'250']);
+
+CampaignHasWppGroup::create(['campaign_id' => 1,'name_group'=>'#120 Aniversario MR INS','redirect_link'=>'https://chat.whatsapp.com/Iay1uqP5i0i8YfZXRRqqs9','order'=>'10', 'max_click'=>'250']);
+
+CampaignHasWppGroup::create(['campaign_id' => 1,'name_group'=>'#121 Aniversario MR INS','redirect_link'=>'https://chat.whatsapp.com/KHsImExoXbo64IDCF9dsyM','order'=>'11', 'max_click'=>'250']);
+
+CampaignHasWppGroup::create(['campaign_id' => 1,'name_group'=>'#122 Aniversario MR INS','redirect_link'=>'https://chat.whatsapp.com/G3u629CFRGXJ0GVAgLaAgc','order'=>'12', 'max_click'=>'250']);
+
+CampaignHasWppGroup::create(['campaign_id' => 1,'name_group'=>'#123 Aniversario MR INS','redirect_link'=>'https://chat.whatsapp.com/EBb5Od2x2QTAhZStHJxyaM','order'=>'13', 'max_click'=>'250']);
+
+CampaignHasWppGroup::create(['campaign_id' => 1,'name_group'=>'#124 Aniversario MR INS','redirect_link'=>'https://chat.whatsapp.com/ECzX53SezhjDTS4InmcphU','order'=>'14', 'max_click'=>'250']);
+
+CampaignHasWppGroup::create(['campaign_id' => 1,'name_group'=>'#125 Aniversario MR INS','redirect_link'=>'https://chat.whatsapp.com/DBfUnkGQQVY36V0ELvLekj','order'=>'15', 'max_click'=>'250']);
+
+CampaignHasWppGroup::create(['campaign_id' => 1,'name_group'=>'#126 Aniversario MR INS','redirect_link'=>'https://chat.whatsapp.com/KXWWomS9rukFvU85FfNEWJ','order'=>'16', 'max_click'=>'250']);
+
+CampaignHasWppGroup::create(['campaign_id' => 1,'name_group'=>'#127 Aniversario MR INS','redirect_link'=>'https://chat.whatsapp.com/JDm7h3Do2nAEJa4Jgo58wP','order'=>'17', 'max_click'=>'250']);
+
+CampaignHasWppGroup::create(['campaign_id' => 1,'name_group'=>'#128 Aniversario MR INS','redirect_link'=>'https://chat.whatsapp.com/DSbn0CR6NzYBics5Tl2cg6','order'=>'18', 'max_click'=>'250']);
+
+CampaignHasWppGroup::create(['campaign_id' => 1,'name_group'=>'#129 Aniversario MR INS','redirect_link'=>'https://chat.whatsapp.com/DaVOWQOavF74i57BBcPIC1','order'=>'19', 'max_click'=>'250']);
+
+CampaignHasWppGroup::create(['campaign_id' => 1,'name_group'=>'#130 Aniversario MR INS','redirect_link'=>'https://chat.whatsapp.com/CdZ9vzRToUzBQ1dW6jeD9Y','order'=>'20', 'max_click'=>'250']);
+
+CampaignHasWppGroup::create(['campaign_id' => 1,'name_group'=>'#131 Aniversario MR INS','redirect_link'=>'https://chat.whatsapp.com/In1tGPEdjQHKuXJCrODsPK','order'=>'21', 'max_click'=>'250']);
+
+CampaignHasWppGroup::create(['campaign_id' => 1,'name_group'=>'#132 Aniversario MR INS','redirect_link'=>'https://chat.whatsapp.com/BkPyQdW5np09VM39rfGkKL','order'=>'22', 'max_click'=>'250']);
+
+CampaignHasWppGroup::create(['campaign_id' => 1,'name_group'=>'#133 Aniversario MR INS','redirect_link'=>'https://chat.whatsapp.com/Isb5h4eehPCGfYQZGwN6KT','order'=>'23', 'max_click'=>'250']);
+
+CampaignHasWppGroup::create(['campaign_id' => 1,'name_group'=>'#134 Aniversario MR INS','redirect_link'=>'https://chat.whatsapp.com/KawpuZJD8gN7gRSNoOBDkN','order'=>'24', 'max_click'=>'250']);
+
+CampaignHasWppGroup::create(['campaign_id' => 1,'name_group'=>'#135 Aniversario MR INS','redirect_link'=>'https://chat.whatsapp.com/Evte6OhO6SAE2Sv3BODtVO','order'=>'25', 'max_click'=>'250']);
+
+CampaignHasWppGroup::create(['campaign_id' => 1,'name_group'=>'#136 Aniversario MR INS','redirect_link'=>'https://chat.whatsapp.com/HnYKzzNbH0XBzfKbtEylr7','order'=>'26', 'max_click'=>'250']);
+
+CampaignHasWppGroup::create(['campaign_id' => 1,'name_group'=>'#137 Aniversario MR INS','redirect_link'=>'https://chat.whatsapp.com/FWE8s4hF55s3PBvSUOxIgd','order'=>'27', 'max_click'=>'250']);
+
+CampaignHasWppGroup::create(['campaign_id' => 1,'name_group'=>'#138 Aniversario MR INS','redirect_link'=>'https://chat.whatsapp.com/E00gQSXiVsZE88cxRofpYD','order'=>'28', 'max_click'=>'250']);
+
+CampaignHasWppGroup::create(['campaign_id' => 1,'name_group'=>'#139 Aniversario MR INS','redirect_link'=>'https://chat.whatsapp.com/FX9lXrnoEJb5fUPnBjrh5g','order'=>'29', 'max_click'=>'250']);
+
+CampaignHasWppGroup::create(['campaign_id' => 1,'name_group'=>'#140 Aniversario MR INS','redirect_link'=>'https://chat.whatsapp.com/DvsiwKlt7HN4IGuswngUsU','order'=>'30', 'max_click'=>'250']);
+
+CampaignHasWppGroup::create(['campaign_id' => 1,'name_group'=>'#141 Aniversario MR INS','redirect_link'=>'https://chat.whatsapp.com/Jm0mxWQX3Go0jcc0Coabyq','order'=>'31', 'max_click'=>'250']);
+
+CampaignHasWppGroup::create(['campaign_id' => 1,'name_group'=>'#142 Aniversario MR INS','redirect_link'=>'https://chat.whatsapp.com/D96QuPodUEKEdc608ylAsX','order'=>'32', 'max_click'=>'250']);
+
+CampaignHasWppGroup::create(['campaign_id' => 1,'name_group'=>'#143 Aniversario MR INS','redirect_link'=>'https://chat.whatsapp.com/D09JB5UFwdYCoQ84UTR7c8','order'=>'33', 'max_click'=>'250']);
+
+CampaignHasWppGroup::create(['campaign_id' => 1,'name_group'=>'#144 Aniversario MR INS','redirect_link'=>'https://chat.whatsapp.com/JJCYCln17DeIbDc0NxifNj','order'=>'34', 'max_click'=>'250']);
+
+CampaignHasWppGroup::create(['campaign_id' => 1,'name_group'=>'#145 Aniversario MR INS','redirect_link'=>'https://chat.whatsapp.com/DVnxfBK9Xqb04qwP24i43e','order'=>'35', 'max_click'=>'250']);
+
+CampaignHasWppGroup::create(['campaign_id' => 1,'name_group'=>'#146 Aniversario MR INS','redirect_link'=>'https://chat.whatsapp.com/GHTaDspSFAzFQfDxASnXrZ','order'=>'36', 'max_click'=>'250']);
+
+CampaignHasWppGroup::create(['campaign_id' => 1,'name_group'=>'#147 Aniversario MR INS','redirect_link'=>'https://chat.whatsapp.com/DHFkS95iYzb03ge8oZyRSQ','order'=>'37', 'max_click'=>'250']);
+
+CampaignHasWppGroup::create(['campaign_id' => 1,'name_group'=>'#148 Aniversario MR INS','redirect_link'=>'https://chat.whatsapp.com/EnDAJJNzabj5ChanHASk0U','order'=>'38', 'max_click'=>'250']);
+
+CampaignHasWppGroup::create(['campaign_id' => 1,'name_group'=>'#149 Aniversario MR INS','redirect_link'=>'https://chat.whatsapp.com/Jx1NsGMExHs09VVVS0PYHL','order'=>'39', 'max_click'=>'250']);
+
+CampaignHasWppGroup::create(['campaign_id' => 1,'name_group'=>'#150 Aniversario MR INS','redirect_link'=>'https://chat.whatsapp.com/Ha6xEe5y3EP1ZnSvVjAgCh','order'=>'40', 'max_click'=>'250']);
+
+CampaignHasWppGroup::create(['campaign_id' => 2,'name_group'=>'#1 Aniversario MR INS','redirect_link'=>'https://chat.whatsapp.com/HC6blJgy1Yf0fMr7g5qvzh','order'=>'1', 'max_click'=>'250']);
+
+CampaignHasWppGroup::create(['campaign_id' => 2,'name_group'=>'#2 Aniversario MR INS','redirect_link'=>'https://chat.whatsapp.com/JjIKjgwqohs4D3B7TzkoTP','order'=>'2', 'max_click'=>'250']);
+
+CampaignHasWppGroup::create(['campaign_id' => 2,'name_group'=>'#3 Aniversario MR INS','redirect_link'=>'https://chat.whatsapp.com/H3HD4iGLHsfDtYaTjnG7IO','order'=>'3', 'max_click'=>'250']);
+
+CampaignHasWppGroup::create(['campaign_id' => 2,'name_group'=>'#4 Aniversario MR INS','redirect_link'=>'https://chat.whatsapp.com/EvQTt8yBJ4EBKN5pWLmJd5','order'=>'4', 'max_click'=>'250']);
+
+CampaignHasWppGroup::create(['campaign_id' => 2,'name_group'=>'#5 Aniversario MR INS','redirect_link'=>'https://chat.whatsapp.com/LPyxGHrBvr5KYMT9pvApUa','order'=>'5', 'max_click'=>'250']);
+
+CampaignHasWppGroup::create(['campaign_id' => 2,'name_group'=>'#6 Aniversario MR INS','redirect_link'=>'https://chat.whatsapp.com/D5GUXo4RfGB5MznS7jwaeg','order'=>'6', 'max_click'=>'250']);
+
+CampaignHasWppGroup::create(['campaign_id' => 2,'name_group'=>'#7 Aniversario MR INS','redirect_link'=>'https://chat.whatsapp.com/CWx30zjrx7RLVOUM9z5RCh','order'=>'7', 'max_click'=>'250']);
+
+CampaignHasWppGroup::create(['campaign_id' => 2,'name_group'=>'#8 Aniversario MR INS','redirect_link'=>'https://chat.whatsapp.com/L4fYwrGBDJc4uYT0ed9Lxh','order'=>'8', 'max_click'=>'250']);
+
+CampaignHasWppGroup::create(['campaign_id' => 2,'name_group'=>'#9 Aniversario MR INS','redirect_link'=>'https://chat.whatsapp.com/LgYmB89aUeHCFJ06aPTvII','order'=>'9', 'max_click'=>'250']);
+
+CampaignHasWppGroup::create(['campaign_id' => 2,'name_group'=>'#10 Aniversario MR INS','redirect_link'=>'https://chat.whatsapp.com/EGmEQVKFHAqFjqiL1zIis3','order'=>'10', 'max_click'=>'250']);
+
+CampaignHasWppGroup::create(['campaign_id' => 2,'name_group'=>'#11 Aniversario MR INS','redirect_link'=>'https://chat.whatsapp.com/G2F2xxo7JfyAVnxpsvjorl','order'=>'11', 'max_click'=>'250']);
+
+CampaignHasWppGroup::create(['campaign_id' => 2,'name_group'=>'#12 Aniversario MR INS','redirect_link'=>'https://chat.whatsapp.com/LDMpDYwLKp2FM7TngVcfA6','order'=>'12', 'max_click'=>'250']);
+
+CampaignHasWppGroup::create(['campaign_id' => 2,'name_group'=>'#13 Aniversario MR INS','redirect_link'=>'https://chat.whatsapp.com/HLN8iWKgE0Y9jYfSFbRb0E','order'=>'13', 'max_click'=>'250']);
+
+CampaignHasWppGroup::create(['campaign_id' => 2,'name_group'=>'#14 Aniversario MR INS','redirect_link'=>'https://chat.whatsapp.com/LQfyBBYRCWVCjnct2M55O7','order'=>'14', 'max_click'=>'250']);
+
+CampaignHasWppGroup::create(['campaign_id' => 2,'name_group'=>'#15 Aniversario MR INS','redirect_link'=>'https://chat.whatsapp.com/CCV33sMGgOh42zFEHpHczs','order'=>'15', 'max_click'=>'250']);
+
+CampaignHasWppGroup::create(['campaign_id' => 2,'name_group'=>'#16 Aniversario MR INS','redirect_link'=>'https://chat.whatsapp.com/Ly8pZQCCZlt0Wcf3tghZu4','order'=>'16', 'max_click'=>'250']);
+
+CampaignHasWppGroup::create(['campaign_id' => 2,'name_group'=>'#17 Aniversario MR INS','redirect_link'=>'https://chat.whatsapp.com/HvW5ek4MwWiK6YcQEdhuxR','order'=>'17', 'max_click'=>'250']);
+
+CampaignHasWppGroup::create(['campaign_id' => 2,'name_group'=>'#18 Aniversario MR INS','redirect_link'=>'https://chat.whatsapp.com/EgaXqhDBuxN8nqD5z3yx50','order'=>'18', 'max_click'=>'250']);
+
+CampaignHasWppGroup::create(['campaign_id' => 2,'name_group'=>'#19 Aniversario MR INS','redirect_link'=>'https://chat.whatsapp.com/Kg73VVvfbxC9HikUyb6Q3y','order'=>'19', 'max_click'=>'250']);
+
+CampaignHasWppGroup::create(['campaign_id' => 2,'name_group'=>'#20 Aniversario MR INS','redirect_link'=>'https://chat.whatsapp.com/LcE08jwkjmE9dprdOvIfcn','order'=>'20', 'max_click'=>'250']);
+
+CampaignHasWppGroup::create(['campaign_id' => 2,'name_group'=>'#21 Aniversario MR INS','redirect_link'=>'https://chat.whatsapp.com/FVvarEXFUnG76ebTlMnZWf','order'=>'21', 'max_click'=>'250']);
+
+CampaignHasWppGroup::create(['campaign_id' => 2,'name_group'=>'#22 Aniversario MR INS','redirect_link'=>'https://chat.whatsapp.com/BhgYd635Id1JGxAhGnyxdU','order'=>'22', 'max_click'=>'250']);
+
+CampaignHasWppGroup::create(['campaign_id' => 2,'name_group'=>'#23 Aniversario MR INS','redirect_link'=>'https://chat.whatsapp.com/KXdk2r07DYE2Tp6eVIzM2j','order'=>'23', 'max_click'=>'250']);
+
+CampaignHasWppGroup::create(['campaign_id' => 2,'name_group'=>'#24 Aniversario MR INS','redirect_link'=>'https://chat.whatsapp.com/HerLw8n7Gu99t60BMSrm2z','order'=>'24', 'max_click'=>'250']);
+
+CampaignHasWppGroup::create(['campaign_id' => 2,'name_group'=>'#25 Aniversario MR INS','redirect_link'=>'https://chat.whatsapp.com/ISR8PQo2wnJK1xHeBVfLrG','order'=>'25', 'max_click'=>'250']);
+
+CampaignHasWppGroup::create(['campaign_id' => 2,'name_group'=>'#26 Aniversario MR INS','redirect_link'=>'https://chat.whatsapp.com/ICUWZGZoKpl0kcVqsylSpQ','order'=>'26', 'max_click'=>'250']);
+
+CampaignHasWppGroup::create(['campaign_id' => 2,'name_group'=>'#27 Aniversario MR INS','redirect_link'=>'https://chat.whatsapp.com/IN8mIRVC7Bu4sYO5R1mXV1','order'=>'27', 'max_click'=>'250']);
+
+CampaignHasWppGroup::create(['campaign_id' => 2,'name_group'=>'#28 Aniversario MR INS','redirect_link'=>'https://chat.whatsapp.com/LN35TFph6CYLT1YtcqeZpX','order'=>'28', 'max_click'=>'250']);
+
+CampaignHasWppGroup::create(['campaign_id' => 2,'name_group'=>'#29 Aniversario MR INS','redirect_link'=>'https://chat.whatsapp.com/IZEpZsJBTOB9AaQOBnKFDL','order'=>'29', 'max_click'=>'250']);
+
+CampaignHasWppGroup::create(['campaign_id' => 2,'name_group'=>'#30 Aniversario MR INS','redirect_link'=>'https://chat.whatsapp.com/LujKKwb4dB3AqxN1DIAKsl','order'=>'30', 'max_click'=>'250']);
+
+CampaignHasWppGroup::create(['campaign_id' => 2,'name_group'=>'#31 Aniversario MR INS','redirect_link'=>'https://chat.whatsapp.com/Bouu8EmXmRA1rMkk1nfZ1f','order'=>'31', 'max_click'=>'250']);
+
+CampaignHasWppGroup::create(['campaign_id' => 2,'name_group'=>'#32 Aniversario MR INS','redirect_link'=>'https://chat.whatsapp.com/G2NMMPmnuCw7T2MbZ5iW4V','order'=>'32', 'max_click'=>'250']);
+
+CampaignHasWppGroup::create(['campaign_id' => 2,'name_group'=>'#33 Aniversario MR INS','redirect_link'=>'https://chat.whatsapp.com/DKk9q2akrneG4W5EPjjAx9','order'=>'33', 'max_click'=>'250']);
+
+CampaignHasWppGroup::create(['campaign_id' => 2,'name_group'=>'#34 Aniversario MR INS','redirect_link'=>'https://chat.whatsapp.com/BBx8k0lO2KNHjj9EbsLeOt','order'=>'34', 'max_click'=>'250']);
+
+CampaignHasWppGroup::create(['campaign_id' => 2,'name_group'=>'#35 Aniversario MR INS','redirect_link'=>'https://chat.whatsapp.com/Ks0hWQS7AZC8KkfmuDdvhO','order'=>'35', 'max_click'=>'250']);
+
+CampaignHasWppGroup::create(['campaign_id' => 2,'name_group'=>'#36 Aniversario MR INS','redirect_link'=>'https://chat.whatsapp.com/F2WZKk34nljBUyN3GD37X7','order'=>'36', 'max_click'=>'250']);
+
+CampaignHasWppGroup::create(['campaign_id' => 2,'name_group'=>'#37 Aniversario MR INS','redirect_link'=>'https://chat.whatsapp.com/IRFI1ywIsof9ds5pJN7DzW','order'=>'37', 'max_click'=>'250']);
+
+CampaignHasWppGroup::create(['campaign_id' => 2,'name_group'=>'#38 Aniversario MR INS','redirect_link'=>'https://chat.whatsapp.com/LkwmImSZdwwKg7rEsTFhUS','order'=>'38', 'max_click'=>'250']);
+
+CampaignHasWppGroup::create(['campaign_id' => 2,'name_group'=>'#39 Aniversario MR INS','redirect_link'=>'https://chat.whatsapp.com/LUTK7lPwwj9HiB63z7ofO4','order'=>'39', 'max_click'=>'250']);
+
+CampaignHasWppGroup::create(['campaign_id' => 2,'name_group'=>'#40 Aniversario MR INS','redirect_link'=>'https://chat.whatsapp.com/KdjgHk1GQgp7I7Y7nKPrhl','order'=>'40', 'max_click'=>'250']);
+
+CampaignHasWppGroup::create(['campaign_id' => 2,'name_group'=>'#41 Aniversario MR INS','redirect_link'=>'https://chat.whatsapp.com/ByLwrURENa2DB0IeOrJm6h','order'=>'41', 'max_click'=>'250']);
+
+CampaignHasWppGroup::create(['campaign_id' => 2,'name_group'=>'#42 Aniversario MR INS','redirect_link'=>'https://chat.whatsapp.com/K2k59drTExRGBi4xGgUQ4J','order'=>'42', 'max_click'=>'250']);
+
+CampaignHasWppGroup::create(['campaign_id' => 2,'name_group'=>'#43 Aniversario MR INS','redirect_link'=>'https://chat.whatsapp.com/GpISgUdgWt1AYmKSuY49gG','order'=>'43', 'max_click'=>'250']);
+
+CampaignHasWppGroup::create(['campaign_id' => 2,'name_group'=>'#44 Aniversario MR INS','redirect_link'=>'https://chat.whatsapp.com/JVWRHVNAAKO2tUH1PlwIV9','order'=>'44', 'max_click'=>'250']);
+
+CampaignHasWppGroup::create(['campaign_id' => 2,'name_group'=>'#45 Aniversario MR INS','redirect_link'=>'https://chat.whatsapp.com/EXAxxPTTDQALE0dxHthQhR','order'=>'45', 'max_click'=>'250']);
+
+CampaignHasWppGroup::create(['campaign_id' => 2,'name_group'=>'#46 Aniversario MR INS','redirect_link'=>'https://chat.whatsapp.com/BbPAxzAmt1lB6hKUrbz9MA','order'=>'46', 'max_click'=>'250']);
+
+CampaignHasWppGroup::create(['campaign_id' => 2,'name_group'=>'#47 Aniversario MR INS','redirect_link'=>'https://chat.whatsapp.com/J1mY3jPJCcn0ZA1vGzEp84','order'=>'47', 'max_click'=>'250']);
+
+CampaignHasWppGroup::create(['campaign_id' => 2,'name_group'=>'#48 Aniversario MR INS','redirect_link'=>'https://chat.whatsapp.com/K8v9rDfrBtm2pqbfh7UbgD','order'=>'48', 'max_click'=>'250']);
+
+CampaignHasWppGroup::create(['campaign_id' => 2,'name_group'=>'#49 Aniversario MR INS','redirect_link'=>'https://chat.whatsapp.com/G8XTadNTxkqGVNwyo5PEOg','order'=>'49', 'max_click'=>'250']);
+
+CampaignHasWppGroup::create(['campaign_id' => 2,'name_group'=>'#50 Aniversario MR INS','redirect_link'=>'https://chat.whatsapp.com/L4ODyabcl7e0EvF9Ug3gXE','order'=>'50', 'max_click'=>'250']);
+
     }
 }
