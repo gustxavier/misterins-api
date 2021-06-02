ca73e3b6f8b8b335a3c4cbffbe0cf298f9aa0a26
Gustavo Xavier <gustasv00@gmail.com>
2021-06-02 19:21:51 -0300

    .dev

---
+++ database/migrations/2021_06_02_125736_create_user_has_classes_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserHasClassesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_has_classes', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->unsignedInteger('class_id');
+            $table->foreign('class_id')->references('id')->on('course_has_classes');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_has_classes');
+    }
+}
