cd9758813bbe723368dfd43e9ea832d3455e641b
gustxavier <gustasv00@gmail.com>
2021-07-02 16:29:51 -0300

    dev.

--- app/User.php
+++ app/User.php
@@ -54,7 +54,7 @@ class User extends Authenticatable implements JWTSubject
     }
 
     public function create($fields)
-    {
+    {   
         return parent::create([
             'name' => $fields['name'],
             'email' => $fields['email'] ,
