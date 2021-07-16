080796ad3992f33be5a697bcee06bf47625fb067
gustxavier <gustasv00@gmail.com>
2021-07-16 17:39:25 -0300

    dev.

--- database/migrations/2021_07_05_190848_create_activity_log_table.php
+++ database/migrations/2021_07_05_190848_create_activity_log_table.php
@@ -1,18 +1,20 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class CreateActivityLogTable extends Migration
 {
     /**
      * Run the migrations.
+     *
+     * @return void
      */
     public function up()
     {
         Schema::create('activitylog', function (Blueprint $table) {
-            $table->id('id');
+            $table->id();
             $table->string('log_name')->nullable();
             $table->text('description');
             $table->nullableMorphs('subject', 'subject');
